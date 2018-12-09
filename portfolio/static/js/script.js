a00f8e81855c2b5db3c1efd25481f22c2cd2eccf
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-12-09 22:59:28 +0300

    Animejs added

--- portfolio/static/js/script.js
+++ portfolio/static/js/script.js
@@ -1,28 +1,114 @@
-$(document).ready(function(){
-  $("#nav-home").click(function(){
-    $("#skills").hide();
-    $("#contact").hide();
-    $("#projects").hide();
-    $("#home").show();
-  });
-  $("#nav-skills").click(function(){
-    $("#home").hide();
-    $("#projects").hide();
-    $("#contact").hide();
-    $("#skills").show();
-  });
-  $("#nav-projects").click(function(){
-    $("#home").hide();
-    $("#skills").hide();
-    $("#contact").hide();
-    $("#projects").show();
-  });
-  $("#nav-contact").click(function(){
-    $("#home").hide();
-    $("#skills").hide();
-    $("#projects").hide();
-    $("#contact").show();
-  });
-
-
+// fullPage
+new fullpage('#fullpage', {
+  sectionsColor: ['black', 'darkorange', '#C0C0C0', '#ADD8E6'],
 });
+
+
+
+
+// Ml6
+// Wrap every letter in a span
+$('.ml6 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 1000,
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  }).add({
+    targets: '.ml6',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 3400,
+  });
+
+// Ml2
+// Wrap every letter in a span
+$('.ml2').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 750,
+    delay: function(el, i) {
+      return 70*i;
+    }
+  }).add({
+    targets: '.ml2',
+    opacity: 1,
+    duration: 600,
+    easing: "easeOutExpo",
+    delay: 3500
+  });
+
+// Ml1
+  // Wrap every letter in a span
+$('.ml1 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: function(el, i) {
+      return 70 * (i+1)
+    }
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: function(el, i, l) {
+      return 80 * (l - i);
+    }
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 3505
+  });
+
+
+//Ml16
+// Wrap every letter in a span
+$('.ml16').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 2500,
+    delay: function(el, i) {
+      return 30 * i;
+    }
+  }).add({
+    targets: '.ml16',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 8000
+  });
